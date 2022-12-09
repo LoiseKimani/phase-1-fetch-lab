e1d050e68d327690227ef27019eb7b4029dc7e37
Loise <loise.kimani@student.moringaschool.com>
2022-12-09 12:09:05 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
   
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then(renderBooks);
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h4 = document.createElement("h4");
+    h4.innerHTML = book.name;
+    main.appendChild(h4);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
